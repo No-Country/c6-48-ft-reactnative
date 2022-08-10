a49cf2744cdc8ee3704ec1db3f6ddac33dfa9ccb
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-10 18:35:09 -0300

    StatusBar o barra de estado estilizada, themeApp creado

--- frontend/audiocommerce/App.js
+++ frontend/audiocommerce/App.js
@@ -2,11 +2,16 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { MenuDrawer } from './src/navegacion/Drawer';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
+import { themeApp } from './src/themeApp/themeApp';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={themeApp.colorBlack}
+      />
       <MenuDrawer />
     </NavigationContainer>
   )
---
+++ frontend/audiocommerce/src/themeApp/themeApp.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export const themeApp= StyleSheet.create({
+    colorPrimary: '#D87D4A',
+    colorSecundary: '#F1F1F1',
+    colorBlack: '#101010'
+})
