67091002df8ccf5abf5b5a2c166786f2ac22125d
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-16 15:43:28 -0300

    se movio la logica parqa obtener los productos al Home

--- frontend/audiocommerce/src/screens/headphonesScreen/HeadphonesScreen.js
+++ frontend/audiocommerce/src/screens/headphonesScreen/HeadphonesScreen.js
@@ -1,45 +1,14 @@
-import React, { useContext, useEffect } from 'react';
-import { Button, ScrollView, Text } from 'react-native';
-import { apiDB } from '../../api/apiDb';
-import { ProductContext } from '../../context/productContext/ProductContext';
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
 
 
 
 export const HeadphonesScreen = () => {
 
-	const context = useContext(ProductContext)
-
-	const dataHeadphones = async () => {
-		const { data } = await apiDB.get('/productos', {
-			params: {
-				limite: 10
-			}
-		});
-		context.addProducts(data.productos, 'headphones')
-
-	}
-
-	useEffect(() => {
-		dataHeadphones()
-	}, [])
-
 
 	return (
 		<ScrollView>
-			<Text>
-				{
-					JSON.stringify(context.productState, null, 4)
-				}
-
-			</Text>
-			<Button
-				title='Add products'
-				onPress={() => dataHeadphones()}
-			/>
-			<Button
-				title='Add products cart'
-				onPress={() => context.addProductCart('1')}
-			/>
+			<Text>Headphones</Text>
 		</ScrollView>
 
 	)
--- frontend/audiocommerce/src/screens/homeScreen/HomeScreen.js
+++ frontend/audiocommerce/src/screens/homeScreen/HomeScreen.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {Text} from 'react-native';
+import { apiDB } from '../../api/apiDb';
+import { ProductContext } from '../../context/productContext/ProductContext';
 
 export const HomeScreen = () => {
+	
+	const context = useContext(ProductContext)
+
+	const getData = async () => {
+		const { data } = await apiDB.get('/productos', {
+			params: {
+				limite: 10
+			}
+		});
+		context.addProducts(data.productos)
+
+	}
+
+	useEffect(() => {
+		getData()
+		console.log('hey! se obtubo la data')
+	}, [])
+
 	return (
 		<Text>Home</Text>
 	)
