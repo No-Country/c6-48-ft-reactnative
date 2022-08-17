bb297378f99c75627159c878c51cb596a3d047f9
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-17 19:16:33 -0300

    refactorizacion

--- frontend/audiocommerce/src/components/index.js
+++ frontend/audiocommerce/src/components/index.js
@@ -1,8 +1,10 @@
+export { GrayCard } from "./cards/GrayCard";
+
 export { ImageIcon } from "./images/ImageIcon";
 export { ImageCirc } from "./images/ImageCirc";
-export { OrangeCard } from "./home/OrangeCard";
+export { OrangeCard } from "./cards/OrangeCard";
 export { ButtonSeeProduct } from "./buttons/ButtonSeeProduct";
-export { NewProduct } from "./home/NewProduct";
+export { BlackCard } from "./cards/BlackCard";
 export { Loading } from "./loading/Loading";
 export { DrawerContent } from "./drawerContent/DrawerContent";
 export { HeaderApp } from "./header/HeaderApp";
