e353ad9a97f9d126dd8870b654be1f28f4b9d5b6
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-17 19:16:54 -0300

    se agregaron funcionalidades

--- frontend/audiocommerce/src/components/buttons/ButtonSeeProduct.js
+++ frontend/audiocommerce/src/components/buttons/ButtonSeeProduct.js
@@ -2,28 +2,30 @@ import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 import { themeApp } from '../../themeApp/themeApp'
 
-export const ButtonSeeProduct = ({ onPress, backgroundColor }) => {
-  return (
-    <TouchableOpacity
-        style={{
-            backgroundColor,
-            paddingHorizontal: 20,
-            width: 280,
-            height:60,
-            justifyContent:'center'
-        }}
-        onPress={ onPress }
-    >
-        <Text
-            style={{
-                color: 'white',
-                fontSize: 25,
-                textAlign:'center'
+export const ButtonSeeProduct = ({ onPress, containerStyle, textStyle }) => {
 
-            }}
+    return (
+        
+        <TouchableOpacity
+            style={[{
+                paddingHorizontal: 20,
+                backgroundColor: themeApp.colorPrimary,
+                width: 280,
+                height: 60,
+                justifyContent: 'center'
+            }, containerStyle ]}
+            onPress={onPress}
         >
-            SEE PRODUCT
-        </Text>
-    </TouchableOpacity>
-  )
+            <Text
+                style={[{
+                    color: 'white',
+                    fontSize: 25,
+                    textAlign: 'center'
+
+                }, textStyle]}
+            >
+                SEE PRODUCT
+            </Text>
+        </TouchableOpacity>
+    )
 }
