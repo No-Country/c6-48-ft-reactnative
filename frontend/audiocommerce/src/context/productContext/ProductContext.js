6138020533435fd3e20a681cfdfada6003178919
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-24 15:03:30 -0300

    se agregaron funcionalidades al context

--- frontend/audiocommerce/src/context/productContext/ProductContext.js
+++ frontend/audiocommerce/src/context/productContext/ProductContext.js
@@ -33,74 +33,146 @@ export const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
 
-    const [productState, dispatch] = useReducer( productReducer,{
-        products:[],
-        cart:[],
-        productDetails:{},
-        errorMsg:''
+    const [productState, dispatch] = useReducer(productReducer, {
+        products: [],
+        cart: [],
+        productDetails: {},
+        errorMsg: '',
+        isLoading: true,
     })
 
-    const addProducts = ( data )=>{
-        dispatch({type:'addProducts', payload: data })
+    const addProducts = (data) => {
+        dispatch({ type: 'addProducts', payload: data })
     }
 
-    const addProductCart = (id)=>{
+    const addProductCart = (id) => {
 
         dispatch({
-            type:'addProductCart', 
-            payload:{
+            type: 'addProductCart',
+            payload: {
                 id
             }
 
         })
     }
 
-    const addProductDetails = ( product )=>{
+    const addProductDetails = (product) => {
         dispatch({
-            type:'addProductDetails',
+            type: 'addProductDetails',
             payload: product
         })
     }
 
-    const createProduct = async( product )=>{
 
-        const body = JSON.stringify(product);
+    const createProduct = (product, fileToUpload) => {
 
-        try {
-            const { data } = await apiDB.post('/productos', body, {
-                "headers": {
-    
-                    "content-type": "application/json",
-                    
-                    },
-            })
-            return true;
-        } catch (error) {
-            console.log(JSON.stringify(error.response.data.errors, null , 4))
-            dispatch({
-                type: 'addErrorMsg',
-                payload: error.response.data.errors[0].msg
-            })
+
+        if (fileToUpload.uri) {
+
+            const formData = new FormData();
+
+            formData.append('file', fileToUpload);
+
+
+
+            apiDB.postForm('/uploads/productos', formData)
+                .then(resp => {
+
+                    product.img = resp.data.msg;
+
+                    const body = JSON.stringify(product);
+
+                    apiDB.post('/productos', body, {
+                        "headers": {
+
+                            "content-type": "application/json",
+
+                        },
+                    }).then(resp => console.log(`Se creo el producto: ${product.title} con imagen`))
+                        .catch(error => {
+                            console.log(error)
+                            dispatch({
+                                type: 'addErrorMsg',
+                                payload: error.response.data.errors[0].msg
+                            })
+                        })
+                }).catch(error => {
+                    console.log(error)
+                    dispatch({
+                        type: 'addErrorMsg',
+                        payload: error.response.data.errors[0].msg
+                    })
+                });
+
+        } else {
+
+            product.img = 'https://res.cloudinary.com/abel1711/image/upload/v1661199049/qmaemueijjpnyecktfoj.jpg';
+            const body = JSON.stringify(product);
+
+                    apiDB.post('/productos', body, {
+                        "headers": {
+
+                            "content-type": "application/json",
+
+                        },
+                    }).then(resp => console.log(`Se creo el producto: ${product.title}, sin imagen`))
+                        .catch(error => {
+                            console.log(error)
+                            dispatch({
+                                type: 'addErrorMsg',
+                                payload: error.response.data.errors[0].msg
+                            })
+                        })
         }
+
     }
 
-    const removeError = ()=>{
+    const getData = async(setRefreshing)=>{
+        setLoading(true)
+        if(setRefreshing){setRefreshing(true);}
+
+		try {
+			const { data } = await apiDB.get(`/productos`, {
+				params: {
+					limite: 20
+				}
+			});
+			addProducts(data.productos)
+			console.log('hey! se obtubo la data')
+            setLoading(false)
+		} catch (error) {
+			console.log(error)
+		}
+		
+		if(setRefreshing){ setRefreshing(false);}
+    }
+
+    const setLoading = ( value )=>{
+        dispatch({
+            type: 'setLoading',
+            payload: value
+        })
+    }
+
+    const removeError = () => {
         dispatch({
             type: 'removeError'
         })
     }
 
-    const value = {
-        productState,
-        addProducts,
-        addProductCart,
-        addProductDetails,
-        createProduct,
-        removeError
-    }
+
 
     return (
-        <ProductContext.Provider value={ value }>
+        <ProductContext.Provider value={{
+            productState,
+            addProducts,
+            addProductCart,
+            addProductDetails,
+            createProduct,
+            removeError, 
+            getData,
+            setLoading
+        }}>
             {children}
         </ProductContext.Provider>
 
