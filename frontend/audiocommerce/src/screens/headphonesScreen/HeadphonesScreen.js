1d8055b771e9aee7530ccf90399559f1c38cbde1
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-24 15:03:52 -0300

    pull to refresh al headphone screen

--- frontend/audiocommerce/src/screens/headphonesScreen/HeadphonesScreen.js
+++ frontend/audiocommerce/src/screens/headphonesScreen/HeadphonesScreen.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { Loading } from '../../components';
+import { ProductCard } from '../../components/cards/ProductCard';
+import { PullToRefresh } from '../../components/loading/PullToRefresh';
+import { ProductContext } from '../../context/productContext/ProductContext';
 
 
 
-export const HeadphonesScreen = () => {
+export const HeadphonesScreen = ({ navigation }) => {
+
+	const { productState, getData } = useContext(ProductContext);
+
+
+	useEffect(() => {
+		getData()
+	}, [])
+
+
+	if (productState.products.length === 0 && productState.isLoading) {
+		return (
+			<Loading />
+		)
+	}
+
+	const headphones = productState.products.filter( product => product.category === 'headphones');
 
 
 	return (
-		<ScrollView>
-			<Text>Headphones</Text>
-		</ScrollView>
+		<PullToRefresh onRefresh={(setRefresh) => getData(setRefresh)}>
+			{
+				headphones.map(product => (
+					<ProductCard product={product} navigation={navigation} key={product._id} />
+				))
+			}
+		</PullToRefresh>
 
 	)
 }
