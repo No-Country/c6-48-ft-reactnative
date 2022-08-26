ef3b2ae5f79d309cf0a8725c479a7333a00efb59
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-26 20:04:49 -0300

    se crea el modal para el carrito

--- frontend/audiocommerce/src/components/modals/ModalCart.js
+++ frontend/audiocommerce/src/components/modals/ModalCart.js
@@ -1,43 +1,152 @@
-import React, { useContext } from 'react'
-import { Modal, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useEffect, useMemo } from 'react'
+import { Image, Modal, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, useWindowDimensions, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { CartContext } from '../../context/cartContext/CartContext';
+import { convertToCurrency } from '../../helpers/converToCurrency';
+import { getTotalsToPay } from '../../helpers/getTotalsToPay';
 import { themeApp } from '../../themeApp/themeApp';
+import { ModalItem } from './ModalItem';
 
 export const ModalCart = () => {
 
-	const { cartState, setShowCart } = useContext( CartContext );
+	const { cartState, setShowCart, removeAllItems } = useContext(CartContext);
 
+	const { products } = cartState;
 
+	const totals = useMemo(()=>  getTotalsToPay(products), [ products ]);
+	
+	const theCartHaveProducts = (products.length > 0);
+
+	const HEIGHT  = useWindowDimensions().height ;
+
+	useEffect(() => {
+	  if(products.length === 0) {
+		setShowCart(false)
+	  }
+	}, [products])
+	
 
 	return (
 		<Modal
 			visible={cartState.showCart}
 			animationType='fade'
-			transparent={true}
-		// style={{
-
-		// }}
+			transparent
 		>
+
 			<View style={{
 				flex: 1,
-				backgroundColor: 'rgba(0,0,0,.5)',
-				justifyContent:'center',
+				backgroundColor: 'rgba(0,0,0,.3)',
+				justifyContent: 'center',
 				alignItems: 'center'
 			}}>
+
 				<View style={{
 					width: themeApp.widthStd,
-					height: 400,
-					backgroundColor: themeApp.colorWhite
+					height: HEIGHT * .8 ,
+					backgroundColor: themeApp.colorWhite,
+					borderRadius: 10,
+					padding: 30
 				}}>
+					{/* botton to close cart */}
 
-					<Text> MODAL </Text>
 					<TouchableOpacity
-						onPress={() => setShowCart(false)}
+						style={style.buttonClose}
+						onPress={() => setShowCart(!cartState.showCart)}
+						activeOpacity={1}
 					>
-						<Text>cerrar</Text>
+						<Icon name='close-circle-outline' size={35} color='#000' />
 					</TouchableOpacity>
+
+					{/* Title cart */}
+					<View style={style.containerTitle}>
+
+						<Text style={style.title}>CART ( {products.length} ) </Text>
+
+						{
+							theCartHaveProducts && (<TouchableOpacity
+								onPress={() => {
+									removeAllItems()
+									setShowCart(!cartState.showCart)
+								}}
+							>
+								<Text style={style.textRemoveAll}>Remove all</Text>
+							</TouchableOpacity>)
+						}
+
+					</View>
+
+					{/* Items */}
+					<ScrollView
+						showsVerticalScrollIndicator={false}
+					>
+						{
+							products.map((product) => (<ModalItem product={product} key={product._id} />))
+						}
+					</ScrollView>
+
+					<View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+						<Text style={{ color: themeApp.colorGrayDark, fontSize: 20 }}>TOTAL</Text>
+						<Text style={{ color: themeApp.colorBlack, fontSize: 20, fontWeight: '500' }}>{convertToCurrency(totals)}</Text>
+					</View>
+
+					<TouchableOpacity
+						style={theCartHaveProducts ? style.buttonCheckout : style.buttonCheckoutDisabled}
+						disabled={!theCartHaveProducts}
+					>
+						<Text style={[style.textCheckout, !theCartHaveProducts && { color: 'black' }]}>CHECKOUT</Text>
+					</TouchableOpacity>
+
 				</View>
+
+
 			</View>
+
 		</Modal>
 	)
 }
+
+
+const style = StyleSheet.create({
+	title: {
+		fontSize: 30,
+		color: '#000',
+		fontWeight: '600'
+	},
+	containerTitle: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	buttonClose: {
+		position: 'absolute',
+		top: -20,
+		right: -20,
+		backgroundColor: 'white',
+		borderRadius: 100,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10
+	},
+	textRemoveAll: {
+		color: themeApp.colorGrayDark,
+		textDecorationLine: 'underline',
+		fontSize: 22
+	},
+	buttonCheckout: {
+		height: 60,
+		backgroundColor: themeApp.colorPrimary,
+		marginTop: 30,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	buttonCheckoutDisabled: {
+		height: 60,
+		backgroundColor: themeApp.colorWhite,
+		marginTop: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: themeApp.colorBlack,
+		borderWidth: 2
+	},
+	textCheckout: { color: themeApp.colorWhite, fontSize: 20 }
+})
---
+++ frontend/audiocommerce/src/components/modals/ModalItem.js
@@ -0,0 +1,107 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { CartContext } from '../../context/cartContext/CartContext';
+import { convertToCurrency } from '../../helpers/converToCurrency'
+import { themeApp } from '../../themeApp/themeApp'
+
+export const ModalItem = ({ product }) => {
+
+    const { removeProductToCart, changeAmountItems, setShowCart, cartState } = useContext(CartContext);
+
+    const [counter, setCounter] = useState(product.amount);
+
+    useEffect(() => {
+        changeAmountItems(product._id, counter)
+    }, [counter])
+
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 15 }}>
+            {/* Item image */}
+            <View style={{ width: 100, height: 100, backgroundColor: themeApp.colorSecundary, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
+                {/* button to remove item */}
+                <TouchableOpacity
+                    style={style.buttonRemoveItem}
+                    onPress={() => {
+                        removeProductToCart(product._id);
+                    }}
+                >
+                    <Icon name='trash-outline' color='red' size={25} />
+                </TouchableOpacity>
+                <Image
+                    source={{ uri: product.img }}
+                    style={{ width: 80, height: 80, resizeMode: 'contain', borderRadius: 5 }}
+                />
+
+            </View>
+
+            {/* Item price and title */}
+
+            <View style={{
+                width: 155,
+                paddingHorizontal: 15,
+
+            }}>
+                <Text
+                    style={{ color: themeApp.colorBlack, fontSize: 20 }}
+                    adjustsFontSizeToFit
+                    numberOfLines={2}
+                >{product.title}</Text>
+                <Text
+                    style={{
+                        color: themeApp.colorGrayDark,
+                        fontSize: 15
+                    }}
+                >{convertToCurrency(product.price)}</Text>
+
+            </View>
+
+
+            {/* buttons to add or subtract items*/}
+
+            <View style={{ flexDirection: 'row', flex: 1, height: 40 }}>
+
+                <TouchableOpacity
+                    onPress={() => setCounter(prev => prev - 1)}
+                    style={style.counterButtos}
+                    disabled={counter <= 1}
+                >
+                    <Text>-</Text>
+                </TouchableOpacity>
+
+                <View
+                    style={style.counterButtos}
+                >
+                    <Text>{counter}</Text>
+                </View>
+
+                <TouchableOpacity
+                    onPress={() => setCounter(prev => prev + 1)}
+                    style={style.counterButtos}
+                >
+                    <Text>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    counterButtos: {
+        backgroundColor: themeApp.colorSecundary,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonRemoveItem: {
+        backgroundColor: 'white',
+        position: 'absolute',
+        top: -10,
+        right: -10,
+        zIndex: 10,
+        borderColor: 'red',
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 2
+    }
+})
