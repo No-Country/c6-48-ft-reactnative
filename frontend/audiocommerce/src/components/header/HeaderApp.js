092aa2a443052cedc55e5e754c0d8eb8a678f421
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-26 20:04:19 -0300

    se oculta el boton del carrito dependiendo si hay elementos

--- frontend/audiocommerce/src/components/header/HeaderApp.js
+++ frontend/audiocommerce/src/components/header/HeaderApp.js
@@ -8,8 +8,9 @@ import { CartContext } from '../../context/cartContext/CartContext';
 export const HeaderApp = ({ navigation }) => {
 
 
-    const { setShowCart } = useContext(CartContext);
+    const { setShowCart, cartState } = useContext(CartContext);
 
+    
     return (
         <View
             style={styles.headerContainer}>
@@ -17,7 +18,7 @@ export const HeaderApp = ({ navigation }) => {
                 <TouchableOpacity
                     onPress={() => navigation.toggleDrawer()}
                 >
-                    <Icon name='menu-outline' size={30} color={ themeApp.colorWhite } />
+                    <Icon name='menu-outline' size={30} color={themeApp.colorWhite} />
                 </TouchableOpacity>
             </View>
             <View
@@ -28,20 +29,27 @@ export const HeaderApp = ({ navigation }) => {
                     style={styles.headerText}
                 >AudioCommerce</Text>
             </View>
-            <View style={ styles.containerHomeCart } >
+            <View style={styles.containerHomeCart} >
 
                 <TouchableOpacity
-                onPress={()=>navigation.navigate('Home')}
+                    onPress={() =>{ navigation.navigate('Home')}}
                 >
-                    <Icon name='home-outline' size={26} color={ themeApp.colorWhite } />
+                    <Icon name='home-outline' size={26} color={themeApp.colorWhite} />
                 </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={()=> setShowCart(true)}
-                >
-                    <Icon name='cart-outline' size={30} color={ themeApp.colorWhite } />
-                </TouchableOpacity>
+
+
+                {
+
+                    (cartState.products.length >= 1) && (<TouchableOpacity
+                        onPress={() => setShowCart(!cartState.showCart)}
+                    >
+                        <View style={styles.containerNumberItems}><Text style={styles.textNumberItems}>{cartState.products.length}</Text></View>
+                        <Icon name='cart-outline' size={30} color={themeApp.colorWhite} />
+                    </TouchableOpacity>)
+
+                }
             </View>
-        <ModalCart />
+            <ModalCart />
         </View>
     )
 }
@@ -64,17 +72,19 @@ const styles = StyleSheet.create({
         paddingTop: 8,
         fontWeight: '300'
     },
-    containerMenuButton:{
-        flex:1,
+    containerMenuButton: {
+        flex: 1,
         paddingLeft: 10
     },
-    containerHomeCart:{
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'space-evenly',
-        alignItems:'center',
-        width:100,
-        marginLeft:0,
-        paddingRight:10
-    }
+    containerHomeCart: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        width: 100,
+        marginLeft: 0,
+        paddingRight: 10
+    },
+    containerNumberItems: { position: 'absolute', top: -10, right: 9 },
+    textNumberItems: { color: 'red', fontWeight: 'bold' }
 })
