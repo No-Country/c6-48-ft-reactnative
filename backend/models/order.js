6a8fdf631b08060becff7b11f5238e1d87a0c1c1
Abel Aron Amieva <abelamieva@gmail.com>
2022-08-31 19:17:11 -0300

    schema para las ordenes creado

---
+++ backend/models/order.js
@@ -0,0 +1,60 @@
+
+
+const { model, Schema } = require('mongoose');
+
+
+const OrderSchema = Schema({
+    name: {
+        type: String,
+        require: [true, 'The name is required']
+    },
+    email: {
+        type: String,
+        require: [true, 'The email is required'],
+    },
+    phoneNumber: {
+        type: String,
+        require: [true, 'The phone number is required']
+    },
+    address: {
+        type: String,
+        require: [true, 'The address is required']
+    },
+    zipCode: {
+        type: Number,
+        require: [true, 'The Zip code is required']
+    },
+    country: {
+        type: String,
+        require: [true, 'The country is required']
+    },
+    paymentMethod: {
+        type: String,
+        require: [true, 'The payment method is required']
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    cart: [
+        {
+            _id: false,
+            item: {
+                type: Schema.Types.ObjectId,
+                ref: 'Producto'
+            },
+            amount: {
+                type: Number
+            }
+        }
+    ]
+
+})
+
+OrderSchema.methods.toJSON = function () {
+    const { __v, ...rest } = this.toObject();
+
+    return rest;
+}
+
+module.exports = model('Order', OrderSchema);
